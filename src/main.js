ebd1451399febecc1dae1d357562d82a3cc6f43a
Timothy Sturm <timothysturm6@gmail.com>
2021-12-11 22:06:21 -0600

    added light/dark mode setting

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, nativeTheme  } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
@@ -11,6 +11,19 @@ const createWindow = () => {
     })
 
     win.loadFile('html/index.html')
+    
+    ipcMain.handle('dark-mode:toggle', () => {
+      if (nativeTheme.shouldUseDarkColors) {
+        nativeTheme.themeSource = 'light'
+      } else {
+        nativeTheme.themeSource = 'dark'
+      }
+      return nativeTheme.shouldUseDarkColors
+    })
+  
+    ipcMain.handle('dark-mode:system', () => {
+      nativeTheme.themeSource = 'system'
+    })
 }
   
 app.whenReady().then(() => {
