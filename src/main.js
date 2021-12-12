8a7b0e22057b851f4b82d4852472359688d46ed9
Timothy Sturm <timothysturm6@gmail.com>
2021-12-11 23:19:51 -0600

    persist data

--- src/main.js
+++ src/main.js
@@ -1,36 +1,56 @@
-const { app, BrowserWindow, ipcMain, nativeTheme  } = require('electron')
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
 const path = require('path')
+const Store = require('./store.js');
+
+const store = new Store({
+    // We'll call our data file 'user-preferences'
+    configName: 'user-preferences',
+    defaults: {
+        // 800x600 is the default size of our window
+        windowBounds: { width: 800, height: 600 }
+    }
+});
 
 const createWindow = () => {
+    let { width, height } = store.get('windowBounds')
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: width,
+        height: height,
         webPreferences: {
-          preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js')
         }
     })
 
+    win.on('resize', () => {
+        // The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
+        // the height, width, and x and y coordinates.
+        let { width, height } = win.getBounds();
+        // TODO: Minimums
+        // Now that we have them, save them using the `set` method.
+        store.set('windowBounds', { width, height });
+    });
+
     win.loadFile('html/index.html')
-    
+
     ipcMain.handle('dark-mode:toggle', () => {
-      if (nativeTheme.shouldUseDarkColors) {
-        nativeTheme.themeSource = 'light'
-      } else {
-        nativeTheme.themeSource = 'dark'
-      }
-      return nativeTheme.shouldUseDarkColors
+        if (nativeTheme.shouldUseDarkColors) {
+            nativeTheme.themeSource = 'light'
+        } else {
+            nativeTheme.themeSource = 'dark'
+        }
+        return nativeTheme.shouldUseDarkColors
     })
-  
+
     ipcMain.handle('dark-mode:system', () => {
-      nativeTheme.themeSource = 'system'
+        nativeTheme.themeSource = 'system'
     })
 }
-  
+
 app.whenReady().then(() => {
     createWindow()
 
     app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
